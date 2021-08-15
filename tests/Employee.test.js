29a66d7ed040cde8d13f9e46f2b0f68582320738
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-15 21:18:05 +0100

    Add employee.js and test

---
+++ tests/Employee.test.js
@@ -0,0 +1,18 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+    it('Creates an object with a name, id and email property', () => {
+      const employee = new Employee("Joe", "1", "joe@gmail.com");
+  
+      expect(employee.name).toBe("Joe");
+      expect(employee.id).toBe("1");
+      expect(employee.email).toBe("joe@gmail.com");
+    });
+    it('Has functioning getName(), getId() and getEmail() methods', () => {
+        const employee = new Employee("Joe", "1", "joe@gmail.com");
+    
+        expect(employee.getName()).toBe("Joe");
+        expect(employee.getId()).toBe("1");
+        expect(employee.getEmail()).toBe("joe@gmail.com");
+      });
+  });

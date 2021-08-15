74a96e9d8ceb8a246996adf5046c041f27b2c9d6
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-15 19:14:20 +0100

    Add engineer and intern questions

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); 
+const addNew = ["Add new Engineer", "Add new Intern", "Generate HTML"];
 
 const teamMembers = [];
 
-function TeamMember(name, role, id, email, officeNumber) {
-    this.name = name;
-    this.role = role;
-    this.id = id;
-    this.email = email;
-    this.officeNumber= officeNumber;
+function TeamMember(data) {
+    for (const property in data) {
+        if (property == "addNew"){
+            return;
+        }
+        this[property] = data[property];
+    }
 }
 
 inquirer
@@ -33,9 +35,108 @@ inquirer
         message: "Enter the team manager's office number.",
         name: 'officeNumber',
     },
+    {
+        type: 'list',
+        message: "Would you like to add another team member?",
+        choices: addNew,
+        name: 'addNew',
+    },
 ])
   .then((response) => {
-      const manager = new TeamMember(response.name, "Manager", response.id, response.email, response.officeNumber);
+      const manager = new TeamMember(response);
       teamMembers.push(manager);
+      if (response.addNew == "Add new Engineer") {
+          engineerPrompts();
+      } else if (response.addNew == "Add new Intern") {
+          internPrompts();
+      } else if (response.addNew == "Generate HTML") {
+          console.log(teamMembers);
+      }
   }
 );
+
+function engineerPrompts() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Enter the name of your engineer.",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's employee ID.",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's email address.",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's GitHub username.",
+            name: 'github',
+        },
+        {
+            type: 'list',
+            message: "Would you like to add another team member?",
+            choices: addNew,
+            name: 'addNew',
+        },
+    ])
+    .then((response) => {
+        const engineer = new TeamMember(response);
+        teamMembers.push(engineer);
+        if (response.addNew == "Add new Engineer") {
+            engineerPrompts();
+        } else if (response.addNew == "Add new Intern") {
+            internPrompts();
+        } else if (response.addNew == "Generate HTML") {
+            console.log(teamMembers);
+        }
+    });
+}
+
+function internPrompts() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Enter the name of your intern.",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's employee ID.",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's email address.",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Enter the name of the intern's school.",
+            name: 'school',
+        },
+        {
+            type: 'list',
+            message: "Would you like to add another team member?",
+            choices: addNew,
+            name: 'addNew',
+        },
+    ])
+    .then((response) => {
+        const intern = new TeamMember(response);
+        teamMembers.push(intern);
+        if (response.addNew == "Add new Engineer") {
+            engineerPrompts();
+        } else if (response.addNew == "Add new Intern") {
+            internPrompts();
+        } else if (response.addNew == "Generate HTML") {
+            console.log(teamMembers);
+        }
+    });
+}

14fda30c7475a87cbd298e48ae73001462f7769b
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-15 22:31:47 +0100

    Update links in generateHTML and add example index.html file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -49,7 +49,7 @@ function generateEngineers(engineers) {
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${engineer.id}</li>
                         <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-                        <li class="list-group-item">GitHub: <a href="${engineer.github}">${engineer.github}</a></li>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
                       </ul>
                 </div>
             </div> \n`
